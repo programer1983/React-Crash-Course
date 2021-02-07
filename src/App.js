41c9e2e7f45ec710284a2a997d790fd09e6b39db
programer1983 <programer972@gmail.com>
2021-02-07 14:03:05 +0200

    Forwarding through props;

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React from 'react';
 import Header from './components/Header';
 
-//function App() {
-  //return (
-    //<div className="container">
-     // <Header />
-    //</div>
- // );
-//}
-
-class App extends React.Component {
-  render() {
-    return <h1>Hello from a class</h1>
-  }
+function App() {
+  return (
+    <div className="container">
+      <Header title='Hello'/>
+    </div>
+  );
 }
+
 
 export default App;
