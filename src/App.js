45db1906481ad642463a1a0352562db0fca9cc45
programer1983 <programer972@gmail.com>
2021-02-07 18:46:30 +0200

    Data deletion function;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import tasks from './components/data';
 
+const data = tasks;
+
 function App() {
+  const [tasks, setTasks] = useState(data);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+  
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (
+          <Tasks tasks={tasks} onDelete={deleteTask} />
+          ) : (
+              'No Tasks To Shou'
+      )}
     </div>
   );
 }
