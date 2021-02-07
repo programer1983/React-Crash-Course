9e308f8c0072406d607670a7a5dbbe17d20716f1
programer1983 <programer972@gmail.com>
2021-02-07 16:05:29 +0200

    Adding CSS styles;

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const Header = ({ title }) => {
     return (
         <header>
-            <h1 style={headingStyle}>{title}</h1>
+            <h1>{title}</h1>
         </header>
     );
 }
@@ -16,9 +16,9 @@ Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
-const headingStyle = {
-    color: 'red', 
-    backgroundColor: 'black'
-}
+//const headingStyle = {
+    //color: 'red', 
+    //backgroundColor: 'black'
+//}
 
 export default Header;
