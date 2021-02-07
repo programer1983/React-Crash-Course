51a7baa3535c57e59f949205f9d9f8ff41bfe007
programer1983 <programer972@gmail.com>
2021-02-07 19:25:40 +0200

    Create a Toggle Reminder function;

--- src/App.js
+++ src/App.js
@@ -11,12 +11,20 @@ function App() {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => task.id === id ? 
+            {...task, reminder: !task.reminder} : task
+      )
+    )
+  }
   
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-          <Tasks tasks={tasks} onDelete={deleteTask} />
+          <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
           ) : (
               'No Tasks To Shou'
       )}
