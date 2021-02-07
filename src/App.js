5f0a7c367a46b59cbc6f90678ef8dc7b717a60c8
programer1983 <programer972@gmail.com>
2021-02-07 13:43:13 +0200

    Compliance condition;

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function App() {
   const name = 'Dimon'
+  const x = true;
   return (
     <div className="container">
       <h1>Hello from React</h1>
-      <h2>Hello {name}</h2>
+      <h2>Hello {x ? 'Yes' : 'No'}</h2>
     </div>
   );
 }
