5bd19447c6acdd7ee7b772b1bdfdf8a570fc8673
programer1983 <programer972@gmail.com>
2021-02-09 11:35:29 +0200

    Installing a router, creating a footer, components about;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
-import tasks from './components/data';
+import Footer from './components/Footer';
 import AddTask from './components/AddTask';
+import About from './components/About';
+import tasks from './components/data';
 
 const data = tasks;
 
@@ -86,15 +89,23 @@ function App() {
   
   
   return (
+    <Router>
     <div className="container">
       <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
+        <Route path="/" exact render={(props) => (
+        <>
+          {showAddTask && <AddTask onAdd={addTask} />}
+          {tasks.length > 0 ? (
           <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
           ) : (
-              'No Tasks To Shou'
-      )}
+            'No Tasks To Shou'
+          )} 
+        </>
+      )} />
+      <Route path="/about" component={About} />
+      <Footer />
     </div>
+    </Router>
   );
 }
 
